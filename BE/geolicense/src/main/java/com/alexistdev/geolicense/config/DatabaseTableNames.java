817afafec40dca41c30976fbaa78071829edcfff
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2026-04-19 11:43:58 +0700

    feat(License): Add License entity with validations and unit tests
    - Introduce `License` entity with fields for id, user, licenseType, licenseKey, usedSeats, issuedAt, and expiresAt.
    - Implement validations for data integrity and annotations for soft delete.
    - Add comprehensive unit tests covering serialization, field validations, equality, and base entity behavior.
    - Update `pom.xml` to include test dependencies.
    - Modify `DatabaseTableNames` to include `TB_LICENSE`.
    - Change Hibernate DDL auto setting to `update`.

--- BE/geolicense/src/main/java/com/alexistdev/geolicense/config/DatabaseTableNames.java
+++ BE/geolicense/src/main/java/com/alexistdev/geolicense/config/DatabaseTableNames.java
@@ -4,5 +4,6 @@ public class DatabaseTableNames {
     public static final String TB_PRODUCT = "glo_products";
     public static final String TB_USERS = "glo_users";
     public static final String TB_LICENSE_TYPE = "glo_license_types";
+    public static final String TB_LICENSE = "glo_licenses";
 
 }
