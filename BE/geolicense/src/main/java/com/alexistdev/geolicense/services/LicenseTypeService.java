a236027fa1f07d766e380cd0c6a050b2d8b30bea
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2026-04-29 09:52:51 +0700

    feat(LicenseType): Create licenseTypeRequest and Response classes,Service and Repo with unit test

---
+++ BE/geolicense/src/main/java/com/alexistdev/geolicense/services/LicenseTypeService.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2026.
+ * Project: Geolicense
+ * Author: Alexsander Hendra Wijaya
+ * Github: https://github.com/alexistdev
+ * Email: alexistdev@gmail.com
+ */
+
+package com.alexistdev.geolicense.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class LicenseTypeService {
+}
