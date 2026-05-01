dc41b82fdbf493d06a0ff23d7a9adbabf61b07e3
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2026-05-02 00:45:38 +0700

    feat(Activation-Enum): add AuditLogAction and LicenseStatus Enum

---
+++ BE/geolicense/src/main/java/com/alexistdev/geolicense/models/entity/AuditLogAction.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2026.
+ * Project: Geolicense
+ * Author: Alexsander Hendra Wijaya
+ * Github: https://github.com/alexistdev
+ * Email: alexistdev@gmail.com
+ */
+
+package com.alexistdev.geolicense.models.entity;
+
+public enum AuditLogAction {
+    ACTIVATED,RE_ACTIVATED, VERIFIED, DENIED, REVODED
+}
