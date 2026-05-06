345f8b0e3ff3e3ee7d6f5941ca256f1c3905ba02
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2026-05-06 23:22:58 +0700

    feat(menu-service): Add `MenuService` with methods for adding menus, handling existing menu validation, and response conversion

---
+++ BE/geolicense/src/main/java/com/alexistdev/geolicense/services/MenuService.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2026.
+ * Project: Geolicense
+ * Author: Alexsander Hendra Wijaya
+ * Github: https://github.com/alexistdev
+ * Email: alexistdev@gmail.com
+ */
+
+package com.alexistdev.geolicense.services;
+
+import com.alexistdev.geolicense.dto.request.MenuRequest;
+import com.alexistdev.geolicense.dto.response.MenuResponse;
+import com.alexistdev.geolicense.exceptions.ExistingException;
+import com.alexistdev.geolicense.models.entity.Menu;
+import com.alexistdev.geolicense.models.repository.MenuRepo;
+import com.alexistdev.geolicense.utils.MessagesUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+@Slf4j
+@Service
+public class MenuService {
+
+    private MenuRepo menuRepo;
+    private static final Logger logger = Logger.getLogger(LicenseService.class.getName());
+    private static final String SYSTEM_USER = "System";
+    private final MessagesUtils messagesUtils;
+
+    public MenuService(MenuRepo menuRepo, MessagesUtils messagesUtils) {
+        this.menuRepo = menuRepo;
+        this.messagesUtils = messagesUtils;
+    }
+
+    public MenuResponse addMenu(MenuRequest request){
+        Optional<Menu> foundMenu = menuRepo.findByNameIncludingDeleted(request.getName());
+
+        Menu menuToSave;
+        menuToSave = convertToMenu(request, null);
+        if(foundMenu.isPresent()){
+            Menu existingMenu = foundMenu.get();
+            if(!existingMenu.getIsDeleted()){
+                String message = messagesUtils.getMessage("menu.already.exist", request.getName());
+                logger.warning(message);
+                throw new ExistingException(message);
+            }
+            menuToSave = convertToMenu(request, existingMenu.getId());
+            menuToSave.setDeleted(false);
+        }
+        Menu savedMenu = menuRepo.save(menuToSave);
+        assert savedMenu.getParentId() != null;
+        return MenuResponse.builder()
+                .id(savedMenu.getId().toString())
+                .name(savedMenu.getName())
+                .urlink(savedMenu.getUrlink())
+                .classlink(savedMenu.getClasslink())
+                .icon(savedMenu.getIcon())
+                .sortOrder(String.valueOf(savedMenu.getSortOrder()))
+                .parentId(savedMenu.getParentId().toString())
+                .typeMenu(savedMenu.getTypeMenu())
+                .code(savedMenu.getCode())
+                .build();
+
+    }
+
+    private Menu convertToMenu(MenuRequest request, UUID id) {
+        Menu menu = new Menu();
+        if(id != null) menu.setId(id);
+        menu.setName(request.getName());
+        menu.setUrlink(request.getUrlink());
+        menu.setClasslink(request.getClasslink());
+        menu.setSortOrder(Integer.parseInt(request.getSortOrder()));
+        menu.setIcon(request.getIcon());
+        menu.setTypeMenu(request.getTypeMenu());
+        menu.setCode(request.getCode());
+        menu.setParentId(UUID.fromString(request.getParentId()));
+        menu.setCreatedBy(SYSTEM_USER);
+        menu.setModifiedBy(SYSTEM_USER);
+        menu.setCreatedDate(new java.util.Date());
+        menu.setModifiedDate(new java.util.Date());
+        return menu;
+    }
+}
