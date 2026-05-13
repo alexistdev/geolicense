94fcab4d38a8197956e1fa13aca41cbeb9051054
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2026-05-14 05:38:47 +0700

    refactor(security): remove redundant exception declaration in security chain

--- BE/geolicense/src/main/java/com/alexistdev/geolicense/security/config/SecurityConfiguration.java
+++ BE/geolicense/src/main/java/com/alexistdev/geolicense/security/config/SecurityConfiguration.java
@@ -28,7 +28,7 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req
