3ea00bcce65626b2ce57e55d63962dbf5d3ccdb3
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2026-05-14 23:06:35 +0700

    feat(auth): include full name in login response
    - Add `fullName` field to `AuthLoginResponse` for enhanced user context.
    - Update `AuthService` to populate `fullName` during authentication.
    - Adjust tests to verify `fullName` in response payload.
    - Refactor imports and remove unused variables in `AuthController`.

--- BE/geolicense/src/main/java/com/alexistdev/geolicense/dto/response/AuthLoginResponse.java
+++ BE/geolicense/src/main/java/com/alexistdev/geolicense/dto/response/AuthLoginResponse.java
@@ -1,8 +1,5 @@
 package com.alexistdev.geolicense.dto.response;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +14,7 @@ import java.util.List;
 public class AuthLoginResponse {
     private String id;
     private String sessionToken;
+    private String fullName;
     private String role;
     private List<MenuResponse> menus;
     private String homeURL;
